949661a5146eb38d51b22e82aa04352768a934ca
podhmo <ababjam61+github@gmail.com>
2017-10-07 06:26:48 +0900

    remove --markers

--- testmarker/setupcmd.py
+++ testmarker/setupcmd.py
@@ -3,7 +3,6 @@ from setuptools.command.test import test as _test
 
 class test(_test):
     user_options = _test.user_options + [
-        ("markers", None, "listing registered markers"),
         ("ignore=", None, "ignore marked"),
         ("only=", None, "only marked"),
     ]
@@ -14,19 +13,13 @@ class test(_test):
         self.markers = False
         super().initialize_options()
 
-    def run_listing_marker(self, markers):
-        for m in markers:
-            print(m.name, "--", m.description)
-
     def get_markers(self):
         from testmarker import markers
         return markers
 
     def run(self):
         markers = self.get_markers()
-        if self.markers:
-            return self.run_listing_markers(markers)
-        elif self.only is not None:
+        if self.only is not None:
             markers.only([x.strip() for x in self.only.split(",")])
         elif self.ignore is not None:
             markers.ignore([x.strip() for x in self.ignore.split(",")])
