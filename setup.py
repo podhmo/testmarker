735ecc9904b1ac6e2d5638d4121b8a98be74443f
podhmo <ababjam61+github@gmail.com>
2017-10-07 21:29:13 +0900

    bump version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ testing_extras = tests_require + []
 
 setup(
     name='testmarker',
-    version='0.1.0',
+    version='0.1.1',
     description='marker for unittest (stdlib)',
     long_description=README + '\n\n' + CHANGES,
     classifiers=[
